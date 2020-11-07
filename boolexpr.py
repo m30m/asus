5df8f14e2e838a1e89bfa8ac125633700f66d179
rouzbeh.hasheminezhad <rouzbeh.hasheminezhad@gess.ethz.ch>
2020-11-07 22:45:39 +0100

    boolexpr modified for test

--- boolexpr.py
+++ boolexpr.py
@@ -3,27 +3,30 @@ from app import device_ids
 
 class BooleanExpression():
     def __init__(self, rule, value, operator):
-        self.lhs = rule
-        self.rhs = value
+        self.rule = rule
+
         self.op = operator
         if rule == 0:
-            self.left_value = 0
+            self.lhs = 0
         else:
-            self.left_value = device_ids[rule].get_state()
+            self.lhs = device_ids[rule].get_state()
         #self.left_value = device_ids[rule].get_state()
-        self.right_value = self.rhs["value"]
+        self.rhs = value
+
+    def __str__(self):
+        return str(self.lhs) + str(self.op) + str(self.rhs)
 
     def evaluate(self):
         if self.op == "=":
-            return self.left_value == self.right_value
+            return self.lhs == self.rhs
         elif self.op == "<":
-            return self.left_value < self.right_value
+            return self.lhs < self.rhs
         elif self.op == ">":
-            return self.left_value > self.right_value
+            return self.lhs > self.rhs
         elif self.op == "<=":
-            return self.left_value <= self.right_value
+            return self.lhs <= self.rhs
         elif self.op == ">=":
-            return self.left_value <= self.right_value
+            return self.lhs <= self.rhs
 
     def update(self):
-        self.left_value = device_ids[self.lhs].get_state()
+        self.lhs = device_ids[self.lhs].get_state()
